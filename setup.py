e64cc3fbbd4a60a0e3efc6ac8c8bd6b1339adb4b
GitHub Action <dev.flojud@gmail.com>
2024-03-13 12:31:14 +0000

    Bump version: 0.9.0 → 1.0.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="IMAPArchiver",
-    version="0.9.0",
+    version="1.0.0",
     packages=find_packages("src"),
     package_dir={"": "src"},
     install_requires=[],
